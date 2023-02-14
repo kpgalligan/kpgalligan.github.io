5ab668b876d2db24f8b071ccaa7ec9e78516da3d
Kevin Galligan <kgalligan@gmail.com>
2023-02-14 07:32:12 -0500

    GitHub pages config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -39,6 +39,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'touchlab', // Usually your GitHub org/user name.
   projectName: touchlabConfig.docusaurusConfig.projectName, // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
